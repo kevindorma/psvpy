d21e9530d40b7172a2b504327812d3e17cfb3135
Kevin Dorma <kevin@kevindorma.ca>
2021-02-16 14:47:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Kevin Dorma',                   # Type in your name
   author_email = 'kevin@kevindorma.ca',      # Type in your E-Mail
   url = 'https://github.com/kevindorma/psvpy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kevindorma/psvpy/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kevindorma/psvpy/archive/psvpy_0.0.tar.gz',    # I explain this later on
   keywords = ['PSV', 'engineering', 'safety'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
