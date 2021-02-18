0a754f1b93fa615806cda89eef9978827d9646b0
Kevin Dorma <kevin@kevindorma.ca>
2021-02-17 20:51:36 -0700

    try again

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from distutils.core import setup
 setup(
   name = 'psvpy',         # How you named your package folder (MyLib)
-  version = '0.0',      # Start with a small number and increase it with every change you make
+  version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'PSV calculations',   # Give a short description about your library
   author = 'Kevin Dorma',                   # Type in your name
   author_email = 'kevin@kevindorma.ca',      # Type in your E-Mail
   url = 'https://github.com/kevindorma/psvpy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kevindorma/psvpy/archive/psvpy_0.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kevindorma/psvpy/archive/psvpy_0.1.tar.gz',    # I explain this later on
   keywords = ['PSV', 'engineering', 'safety'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
-          'math',
           'pandas',
           'scipy',
       ],
